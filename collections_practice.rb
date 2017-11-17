ef06cf95e7b4709b35e2e0d3a7b22872605c4abc
Jacqueline Lee <anginblu@gmail.com>
2017-11-17 02:48:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,8 +41,8 @@ def reverse_array(array)
   count = 0
   array_new = array
   until count = array.count
-    array_new.push(array[0])
-    array.shift(array[0])
+    array.push(array_new[0])
+    array.shift(array_new[0])
     count += 1
   end
   array_new
