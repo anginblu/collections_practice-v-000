afac0e6ea1182b44a359be4df8be91dc69d455bd
Jacqueline Lee <anginblu@gmail.com>
2017-11-17 02:36:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.size == b.size
+      0
+    elsif a.size < b.size
+      -1
+    elsif a.size > b.size
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array_new = array
+  first = array [1]
+  second = array [2]
+  array_new [1] = second
+  array_new [2] = first
+  array = array_new
+end
+
+def reverse_array(array)
+  array_new = array
+  count = 0
+  while count < array.size
+    i = array.last
+    array.unshift(i)
+    array.pop
+    count += 1
+  end
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
