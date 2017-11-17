8e857e7e45bb8b8b74a2663e0d6c668dc4bbfc84
Jacqueline Lee <anginblu@gmail.com>
2017-11-17 03:25:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -66,9 +66,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| }
-    unless index = 1
-      element = element + "s"
-    end
+  array.each_with_index.collect{|element, index| element + "s" if index != 1}
   array
 end
