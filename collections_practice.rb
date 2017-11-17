aa5e19708ff2471ddb63648a717b5fd558fa612c
Jacqueline Lee <anginblu@gmail.com>
2017-11-17 03:24:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -68,8 +68,7 @@ end
 def add_s(array)
   array.each_with_index.collect{|element, index| }
     unless index = 1
-      size = element.size - 1
-      element.collect()"s")
+      element + "s"
     end
   array
 end
